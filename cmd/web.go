0326ca5121a192349510e14c62e67842ea954700
Mr Chen <midoks@163.com>
2024-06-16 01:55:33 +0800

    update

--- cmd/web.go
+++ cmd/web.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/urfave/cli"
 )
 
@@ -15,6 +16,12 @@ var Web = cli.Command{
 }
 
 func WebRun(c *cli.Context) error {
-
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
 	return nil
 }
